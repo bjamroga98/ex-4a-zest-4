d49539617379c9c75c2f47e22242da17613f06bd
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-05-16 09:23:32 +0200

    commit

--- ex 4a zest 4/Program.cs
+++ ex 4a zest 4/Program.cs
@@ -10,6 +10,32 @@ namespace ex_4a_zest_4
     {
         static void Main(string[] args)
         {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            Employee employee3 = new Employee();
+            Console.WriteLine("Total; " + Employee.NumberOfEmployees);
+
+            Console.ReadKey();
+        }
+        class Human
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public int Age { get; set; }
+        }
+        class Employee : Human
+        {
+            public string Position { get; set; }
+            public float HourlyRate { get; set; }
+            public static int NumberOfEmployees { get; set; }
+            public float CalculatePayment(int hoursNumber)
+            {
+                return hoursNumber * HourlyRate;
+            }
+            public Employee()
+            {
+                NumberOfEmployees++;
+            }
         }
     }
 }
